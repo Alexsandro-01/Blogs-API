8953e703c967122a6c453f54e120b43bfb2e0bfc
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-07-15 16:07:37 -0300

    feat: adiciona model para a tabela postCategories

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,41 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+function postCategory(sequelize, DataTypes) {
+  const PostCategory = sequelize.define('PostCategory',
+    {
+      postId: {
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      categoryId: {
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      }
+    },
+    { 
+      tableName: 'postCategories',
+      timestamps: false
+    },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = postCategory;
